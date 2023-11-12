4146d3a9a158ea5cf3ea0aef06313fcc37954e77
Fouad <fouadaharchi1@gmail.com>
2023-11-12 19:45:46 +0100

    Haber si ahora si me hace la pull request en esta tercera rama

--- src/EjemploDeClaseVacio.java
+++ src/EjemploDeClaseVacio.java
@@ -3,4 +3,5 @@ public class EjemploDeClaseVacio {
     //Todo lo que estoy haciendo aquí se está guardando en la rama,
     //asi que debo hacer una pull request jdflskdjflsdflkj
     //Espero que al crear esta nueva rama, todo esto se pueda pasar a la rama principal
+    //Este es el útlimo ejemplo para aprender, le doy al marge y se hace la pull request
 }
